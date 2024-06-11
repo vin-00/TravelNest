8408124dc7993b4a53570225d72a90a8118724b1
vinay <vinaykhatri292@gmail.com>
2024-06-11 15:46:02 +0530

    modify

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 const port = 8080;
 const mongoose = require("mongoose");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const dbUrl = process.env.ATLASDB_URL;
 
 const ExpressError = require("./utils/ExpressError");
